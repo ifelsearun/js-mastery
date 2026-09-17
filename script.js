224b88b8bd58cc27ff03861cd47c33377c307ec5
ifelsearun <169469230+ifelsearun@users.noreply.github.com>
2026-09-17 14:30:34 +0000

    index.html - add html file - add html boilerplate - add script tag - perform addition of 2 numbers - perform add of 6 numbers
    script.js
    - add js file
    - add let,const and var variables
    - add print output console.log(input)
    "

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Welcome, to my soul society!");
+
+let first_name = "Aizen";
+let last_name = "Sosuke";
+
+console.log(first_name);
+console.log(last_name);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age)
+
+//can't be re-assign the const variable
+const pi = 3.14;
+console.log(pi);
+
+var power = "Bankai";
+console.log(power);
+ 
+power = "Kyokasuigetsu";
+console.log(power);
+
+//BODMAS
+console.log((3 + 2) - 76 * (1 + 1));
+
